e873e909f6778bfee6749e3e8a31f000ef1e17e6
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-10-18 10:44:33 -0700

    STEP 4 -- update, added Sections around S, O

--- src/App.js
+++ src/App.js
@@ -16,14 +16,38 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+{/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+
   return (
     <div className="container">
-      <Logo />
+
+      <section className="sectionLogo">
+        <Logo />
+      </section>
+      
+      <section className="sectionDisplay">
+        <Display /> 
+      </section>
+
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <div className="containerSpecials">
+          <Specials />
+        </div>
+
+        <section className="sectionNumbers">
+          <div className="containerNumbers">
+            <Numbers />
+          </div>
+        </section>
+
+        <section className="sectionOperators">
+          <div className="containerOperators">
+            <Operators />
+          </div>
+        </section>
+
       </div>
     </div>
   );
 }
-
 export default App;
